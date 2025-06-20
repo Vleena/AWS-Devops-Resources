a2605f16e31783b314164ea01eecaabaad1735a6
root <root@invrhel905.informatica.com>
2025-06-20 18:04:56 +0530

    Linux automation

---
+++ Linux-Scripts/directories.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+##########################
+# Author: Vleena
+# Description: create a one directory, inside which 4 direcories with 777 permission
+# version: v1
+###########################
+# set -x
+
+read -p "Enter the parent directory name: " p
+
+mkdir $p
+
+cd $p
+
+read -p "Enter the 1st child directory name: " c
+
+mkdir $c
+
+read -p "Enter the 2nd child directory name: " c1
+
+read -p "Enter the 3nd child directory name: " c2
+read -p "Enter the 4nd child directory name: " c3
+
+mkdir $c1 $c2 $c3
+
+cd ..
+chmod -R 777 $p
